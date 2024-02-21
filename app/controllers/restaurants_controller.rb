8d390a3c43b4a5a25c9372fd3d263dbebe4dec89
Vasiliki Georgiou <vicky.georgiou2012@gmail.com>
2024-02-21 13:04:51 +0100

    Adding actions for update and delete a restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -18,6 +18,23 @@ class RestaurantsController < ApplicationController
     redirect_to restaurants_path
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(params_restaurant)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path, status: :see_other
+  end
+
+
   private
 
   def params_restaurant
