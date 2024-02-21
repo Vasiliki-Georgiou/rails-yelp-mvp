76d5966df581a9bfd896a3f238d162e2d6d062e9
Vasiliki Georgiou <vicky.georgiou2012@gmail.com>
2024-02-21 17:01:01 +0100

    Adding some style and form validation

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
 
-  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :set_a_restaurant, only: [:show, :edit, :update, :destroy]
 
   def index
     @restaurants = Restaurant.all
@@ -15,16 +15,22 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(params_restaurant)
-    @restaurant.save
-    redirect_to restaurants_path
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
-    @restaurant.update(params_restaurant)
-    redirect_to restaurant_path(@restaurant)
+    if @restaurant.update(params_restaurant)
+        redirect_to restaurant_path(@restaurant)
+    else
+        render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -35,7 +41,7 @@ class RestaurantsController < ApplicationController
 
   private
 
-  def set_restaurant
+  def set_a_restaurant
     @restaurant = Restaurant.find(params[:id])
   end
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,8 @@
 class Restaurant < ApplicationRecord
   CATEGORIES = ["chinese", "italian", "japanese",  "french", "belgian"]
   has_many :reviews, dependent: :destroy
-  validates :name, presence: true, length: { minimum: 3 }
+  validates :name, presence: true, length: { minimum: 3, message: "is not valid. Restaurant name should consist of at least 3 characters" }
+  validates :phone_number, presence: true, length: { minimum: 8, message: "is not valid. Telephone number should have between 8 and 10 digits."}
   validates :address, presence: true
   validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
